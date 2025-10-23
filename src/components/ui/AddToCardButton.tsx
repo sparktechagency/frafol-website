9754b6c49ac32796a29a3444028f440665b31fa8
rajin572 <dirrazin572@gmail.com>
2025-10-23 11:23:03 +0600

    feat: Add conditional rendering for Add To Cart button based on user authorization

--- src/components/ui/AddToCardButton.tsx
+++ src/components/ui/AddToCardButton.tsx
@@ -5,22 +5,28 @@ import { useAppDispatch } from "@/redux/hooks";
 import { addToCart } from "@/redux/features/cart/cartSlice";
 import { IGear } from "@/types";
 import { IoCartOutline } from "react-icons/io5";
+import { useUser } from "@/context/UserContext";
 
 const AddToCardButton = ({ gear }: { gear: IGear }) => {
   const dispatch = useAppDispatch();
+  const userData = useUser();
 
   const handleAddToCart = (data: IGear) => {
     dispatch(addToCart(data));
   };
   return (
-    <ReuseButton
-      variant="secondary"
-      className="!px-6 !py-5 mt-4 flex justify-center items-center gap-2"
-      onClick={() => handleAddToCart(gear)}
-    >
-      <IoCartOutline className="text-2xl" />
-      <p>Add To Cart</p>
-    </ReuseButton>
+    <>
+      {userData?.user?.userId !== gear?.authorId?._id && (
+        <ReuseButton
+          variant="secondary"
+          className="!px-6 !py-5 mt-4 flex justify-center items-center gap-2"
+          onClick={() => handleAddToCart(gear)}
+        >
+          <IoCartOutline className="text-2xl" />
+          <p>Add To Cart</p>
+        </ReuseButton>
+      )}
+    </>
   );
 };
 
