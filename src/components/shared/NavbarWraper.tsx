f7c3932887cc1140c97a615ede23546040d9a692
rajin572 <din.islam.rajin572@gmail.com>
2026-02-18 10:33:52 +0600

    Navbar Notification Issue FIxed

--- src/components/shared/NavbarWraper.tsx
+++ src/components/shared/NavbarWraper.tsx
@@ -1,12 +1,20 @@
 import { fetchWithAuth } from '@/lib/fetchWraper';
 import Navbar from './Navbar';
 import { INotification } from '@/types';
+import { getCurrentUser } from '@/services/AuthService';
 
 const NavbarWraper = async () => {
-    const res = await fetchWithAuth(`/notifications/my-notifications?page=1&limit=6`);
-    const data = await res.json();
-    const notifications: INotification[] = data?.data?.notifications || [];
+    const userData = await getCurrentUser();
+    let notifications: INotification[] = [];
 
+    console.log(userData)
+    if (userData) {
+        const res = await fetchWithAuth(`/notifications/my-notifications?page=1&limit=6`);
+        const data = await res.json();
+        notifications = data?.data?.notifications || [];
+    }
+
+    console.log(notifications)
     return (
         <div>
             <Navbar notifications={notifications} />
